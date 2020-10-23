de476356887e857754af999413338287b94cf0e8
Rezyapkin <rezyapkin@yandex.ru>
2020-10-23 10:40:31 +0300

    complete task4

--- lesson1/task1-4.php
+++ lesson1/task1-4.php
@@ -27,9 +27,33 @@ class Product {
         
 }
 
+class CartItem extends Product {
+    public $quantity;
+
+    function __construct($name, $price, $description = "", $quantity) {
+        parent::__construct($name, $price, $description);
+        $this->quantity = $quantity;
+    }
+
+    public function render() {
+        $sum = $this->price * $this->quantity;
+        return "
+        <div class='cart-item'>
+            <b>{$this->name}</b>
+            <div class='product_price' style='font-weight: bold, font-size: 16px'>{$this->quantity} X {$this->price} &#8381; = {$sum} &#8381;</div>
+            <hr>
+        </div>
+        ";
+    }
+
+}
+
 
 $asus = new Product('Ноутбук Asus', 15000, 'отличный ноут');
 echo $asus->render();
 
 $acer = new Product('Ноутбук Acer', 10000, 'средний ноут');
 echo $acer->render();
+
+echo (new CartItem('Ноутбук Acer', 10000, 'средний ноут', 2))->render();
+echo (new CartItem('Ноутбук Asus', 15000, 'отличный ноут', 3))->render();
