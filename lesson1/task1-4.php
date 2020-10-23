8f336b223771814910154e1ba93542292de90069
Rezyapkin <rezyapkin@yandex.ru>
2020-10-23 10:28:46 +0300

    complete task 1-3

---
+++ lesson1/task1-4.php
@@ -0,0 +1,35 @@
+<?php
+
+//Класс продукта
+class Product {
+    public $name;
+    public $description;
+    public $price;
+    public $images = []; //В данном случае храним url изображений. Также могут быть также ID изображений в галерее
+    public $properties = []; //Характеристики товара - ассоциативный массив.
+
+    function __construct($name, $price, $description = "") {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
+
+    public function render() {
+        return "
+        <div class='product'>
+            <b>{$this->name}</b>
+            <p>{$this->description}</p>
+            <div class='product_price' style='font-weight: bold, font-size: 16px'>{$this->price} &#8381;</div>
+            <hr>
+        </div>
+        ";
+    }
+        
+}
+
+
+$asus = new Product('Ноутбук Asus', 15000, 'отличный ноут');
+echo $asus->render();
+
+$acer = new Product('Ноутбук Acer', 10000, 'средний ноут');
+echo $acer->render();
