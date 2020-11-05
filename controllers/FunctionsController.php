7fd65f02ccecbc8ecb444d79af39096cb0edd227
Rezyapkin <rezyapkin@yandex.ru>
2020-11-05 17:26:47 +0300

    add new example

--- controllers/FunctionsController.php
+++ controllers/FunctionsController.php
@@ -13,8 +13,8 @@ class FunctionsController extends Controller
 
         echo $this->render('functions', []);
         echo '<p>Реализована работа с агрегирующими функциями: count, max, min, agv, sum</p>';
-        echo '<p>Реализована сортировка методом orderBy</p>';    
-        echo '<p>Реализована фильтрация методами where и orWhere</p>';    
+        echo '<p>Реализована сортировка методом orderBy. Может передаваться несколько полей для сортировки, а также использоваться DESC</p>';    
+        echo '<p>Реализована фильтрация методами where и orWhere. Методы могут принимать 2 или 3 параметра. Если параметра 2, то подразумевается, что используется знак "="</p>';    
         echo '<p>Методы возвращающие объекты: find и first. Первый ищет элемент по id, второй возвращает первый результат</p>';    
         echo '<p>Метод get доработан для работы с where, orderby. Также у него появились два необязательный параметра limit и offset</p>';    
 
@@ -24,6 +24,7 @@ class FunctionsController extends Controller
         <li>Products::where('price','>','50000')->max('price');</li>
         <li>Products::where('price','>','50000')->orWhere('id','3')->orderBy('price')->get(5,1);</li>
         <li>Products::where('price','>','50000')->where('price','<','100000')->orderBy('name')->fisrt();</li>  
+        <li>Products::where('price','<','50000')->orderBy('name DESC')->get();</li>
         </ul>";      
         echo '<hr><br>';
         echo Products::where('price','>','50000')->where('price','<','60000')->count();
@@ -33,6 +34,8 @@ class FunctionsController extends Controller
         print_r(Products::where('price','>','50000')->orWhere('id','3')->orderBy('price')->get(5,1));
         echo '<hr><br>';
         var_dump(Products::where('price','>','50000')->where('price','<','100000')->orderBy('name')->first());
+        echo '<hr><br>';
+        var_dump(Products::where('price','<','50000')->orderBy('price DESC')->get());
     
     }
 
